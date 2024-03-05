b63f8dcd1e42753ee13d4acdee84ed5cc1d8007f
Sourabh kale <144787724+sourabhkale214@users.noreply.github.com>
2024-03-05 18:15:42 +0530

    Delete EVidyalaya directory

--- EVidyalaya/src/main/java/com/app/entities/Role.java
+++
@@ -1,6 +0,0 @@
-package com.app.entities;
-
-public enum Role {
-	
-	ROLE_ADMIN,ROLE_STUDENT,ROLE_FACULTY
-}
--- EVidyalaya/src/main/java/com/app/service/ILoginService.java
+++
@@ -1,8 +0,0 @@
-package com.app.service;
-
-import com.app.entities.User;
-
-public interface ILoginService {
-
-	User authenticateUser(String email, String password);
-}
